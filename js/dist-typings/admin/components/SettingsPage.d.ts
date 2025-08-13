0894812d9cb5e723a34ad05505ae71342d515d50
wusong8899 <wusong8899@users.noreply.github.com>
2025-08-13 15:02:52 +0900

    build(admin): update build output and remove type definitions
    - Update admin.js and admin.js.map with new build output
    - Remove dist-typings files for admin components and index
    - Update composer.lock with new content hash

--- js/dist-typings/admin/components/SettingsPage.d.ts
+++
@@ -1,7 +0,0 @@
-import ExtensionPage from 'flarum/admin/components/ExtensionPage';
-import type Mithril from 'mithril';
-export default class SettingsPage extends ExtensionPage {
-    loading: boolean;
-    oninit(vnode: Mithril.Vnode<unknown, this>): void;
-    content(vnode: Mithril.VnodeDOM<unknown, this>): JSX.Element;
-}
